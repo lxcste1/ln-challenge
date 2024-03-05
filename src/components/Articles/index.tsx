ca5511e7525fa281a6f64b3bfc0dce073d9a12ab
Lucas <lucas.tello@pinapp.tech>
2024-03-05 19:59:14 -0300

    development of the cards was completed

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,17 +1,39 @@
 import React from 'react'
+
 import { useQuery } from 'react-query'
 import FetchArticles from '@/api/fetchArticles'
+
+import Card from '../Card'
 
 export default function Articles() {
 
     const { data } = useQuery(
         ['articles'],
         async () => await FetchArticles()
-      )
+    )
 
-    console.log(data)
+    const mainArticle = data?.filter((article) => {
+        return article.type == 'main'
+    })
+
+    const secondaryArticle = data?.filter((article) => {
+        return article.type == 'secondary'
+    })
 
     return (
-        <div>Articles</div>
+        <div className='flex flex-wrap lg:min-w-[925px] md:min-w-[680px] lg:mt-[10px] justify-between'>
+            <div>
+                {mainArticle?.map((e, i) => (
+                    <Card key={i} info={e}/>
+                ))}
+            </div>
+            <div className="border-l-[1px] border-[#CCCCCC]"></div>
+            <div>
+                {secondaryArticle?.map((e, i) => (
+                    <Card key={i} info={e} />
+                ))}
+            </div>
+
+        </div>
     )
 }
