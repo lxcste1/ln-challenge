6aee5e9d3a28e0cf2a59e5ed88208de12c05abf9
Lucas <lucas.tello@pinapp.tech>
2024-03-06 15:15:31 -0300

    fix borders in main container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
     <main
-      className={`flex min-h-screen flex-col items-center xl:justify-between lg:p-24 md:border-dashed md:border-2`}
+      className={`flex min-h-screen flex-col items-center xl:justify-between lg:p-24`}
     >
       <h1 className={`border-b-[1px] border-[#CCCCCC] lg:w-[925px] md:w-[680px] w-[360px] font-['suecaslab-bold'] text-left text-[24px] lg:pb-2 lg:mb-3 p-2 m-3`}>Focal Izquierdo</h1>
       <Articles />
